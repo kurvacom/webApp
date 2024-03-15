5b0d3e5c5ec3c6e0499adaa6846915f7f23aae34
Petar Ivanov <petarivanov03it@gmail.com>
2024-03-15 20:13:02 +0200

    added responsive navbar

--- LegalTranslation/LegalTranslation/obj/Debug/net6.0/LegalTranslation.AssemblyInfo.cs
+++ LegalTranslation/LegalTranslation/obj/Debug/net6.0/LegalTranslation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegalTranslation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8bbbb44ce23761c5d9c7c6510c7047bd081572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegalTranslation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegalTranslation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
